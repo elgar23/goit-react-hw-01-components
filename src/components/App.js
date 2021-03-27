742658dfb1873f0ee1637f8e8acbea022c1cf46d
elgar23 <superel2303@gmail.com>
2021-03-27 14:27:47 +0200

    react hw

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Profile from './profile/Profile';
+import user from './profile/user.json';
+import Statistics from './Statistics/Statistics';
+import statisticalData from './Statistics/statistical-data.json';
+import FriendList from './FriendList/FriendList';
+import friends from './FriendList/friends.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from './TransactionHistory/transactions.json';
+
+const App = () => (
+  <>
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+    <Statistics title="Upload stats" stats={statisticalData} />
+        <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
+  </>
+);
+    
+export default App;
